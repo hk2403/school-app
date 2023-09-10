fe05313bd6cb286bc47856073cbc438c56ab69ef
Hemanth Kumar <itsmehemanthk@gmail.com.com>
2023-09-10 23:03:37 +0530

    screens are responsive

--- src/app/all-students/all-students.component.ts
+++ src/app/all-students/all-students.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import { StudentsService } from '../students.service';
 
 @Component({
   selector: 'app-all-students',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./all-students.component.css']
 })
 export class AllStudentsComponent {
+
+  public students:any=[];
+  constructor(private _studentsService:StudentsService){
+    _studentsService.getStudents().subscribe(
+      (data:any)=>{
+        this.students=data;
+      },
+      (err:any)=>{
+        alert("Internal Service Error");
+      }
+    )
+  }
+  deleteStudent(id:any){
+    this._studentsService.deleteStudent(id).subscribe(
+      (data:any)=>{
+        alert("Deleted Successfully");
+      },
+      (err:any)=>{
+        alert("Internal Service Error");
+      }
+    )
+  }
+
 
 }
--- src/app/students.service.ts
+++ src/app/students.service.ts
@@ -8,7 +8,14 @@ import { Observable } from 'rxjs';
 export class StudentsService {
   private url:any="https://64b8a34b21b9aa6eb07a012b.mockapi.io/api/students-mini/students";
   constructor(private _httpClient:HttpClient) { }
+
+  getStudents():Observable<any>{
+    return this._httpClient.get(this.url);
+  }
   createStudent(data:any):Observable<any>{
     return this._httpClient.post(this.url,data);
+  }
+  deleteStudent(id:any):Observable<any>{
+    return this._httpClient.delete(this.url+id);
   }
 }
